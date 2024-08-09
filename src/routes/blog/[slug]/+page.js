613f6052f1a84f8717e8937432ee771807984c42
asokmontridental <asokmontridental@gmail.com>
2024-08-09 23:03:57 +0700

    IM SO DONE

--- src/routes/blog/[slug]/+page.js
+++ src/routes/blog/[slug]/+page.js
@@ -1,6 +1,6 @@
 /** @type {import('./$types').PageLoad} */
 export async function load({ params }) {
-	const post = await import(`./${params.slug}.md`);
+	const post = await import(`${params.slug}.md`);
 	const title = post.title;
 	// const content = post.body;
 
