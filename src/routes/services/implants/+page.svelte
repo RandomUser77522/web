1e08148cd79f367586674873acdccd808b96ead0
RandomUser77522 <121331057+RandomUser77522@users.noreply.github.com>
2025-04-07 22:44:54 +0700

    readmore button + text bolding

--- src/routes/services/implants/+page.svelte
+++ src/routes/services/implants/+page.svelte
@@ -2,15 +2,24 @@
 	import { _ } from "svelte-i18n";
 	import { locale } from "svelte-i18n";
 	import '../styles.css'
+	let expandedSections = [];
+	function toggleSection(index) {
+		expandedSections = expandedSections.includes(index) 
+			? expandedSections.filter(i => i !== index)
+			: [...expandedSections, index];
+	}
 </script>
 
 <div class="text-block" id="welcome">
 	<h1 class="title">{$_("implants.title")}</h1>
 	<h2 class="subtitle">{$_("implants.subtitle")}</h2>
 	<img src="../images/implant.jpg" alt="Implant">
-	{#each $_("implants.content") as content}
+	{#each $_("implants.content") as content, index}
 		<h3 class="topic">{content.topic}</h3>
-		<p class="text">{@html content.text}</p>
+		<p class="text" class:expanded={expandedSections.includes(index)}>{@html content.text}</p>
+		<button on:click={() => toggleSection(index)} class="read-more">
+			{expandedSections.includes(index) ? $_("read less") : $_("read more")}
+		</button>
 	{/each}
 </div>
 
